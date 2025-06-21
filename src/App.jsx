05db69cc56386b8e16bfa8183dd157096fde158d
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-21 15:02:08 -0300

    formato de texto

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
-      <main className="px-3 container">
-        <h1 className="text-center mt-5 mb-4">Formulario</h1>
+      <main className="px-3">
+        <h1 className="text-center my-3">Formulario</h1>
         <Formulario />
       </main>
        <footer className="bg-dark text-light text-center py-3">&copy; Todos los derechos reservados</footer>
